79ce44a59401253d78926eb91ce38ad39a4f91fc
AdityaThundersoft <aditya.v@thundersoft.com>
2024-03-11 15:11:37 +0530

    storage classes

---
+++ static.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAXNUM 3
+void sum_up(void);
+
+int main()
+{
+    int count;
+    printf("\n*****static storage*****\n");
+    printf("Key in 3 numbers to be summed ");
+    for(count = 0; count < MAXNUM; count++)
+        sum_up();
+    printf("\n*****COMPLETED*****\n");
+    return 0;
+}
+
+void sum_up(void)
+{
+    static int sum = 0;
+    int num;
+    printf("\nEnter a number: ");
+    scanf("%d", &num);
+    sum += num;
+    printf("\nThe current total is: %d\n", sum);
+
+}
