1953f74534540944a0a4bda2f7d8ad232ffcbc82
AdityaThundersoft <aditya.v@thundersoft.com>
2024-03-11 14:39:29 +0530

    storage classes

---
+++ auto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int func(void)
+{
+    auto int a =5;
+    printf("%d",a);
+}
+
+int main() {
+    // Write C code here
+    int a=10;
+    printf("%d\n",a);
+    func();
+    return 0;
+}
